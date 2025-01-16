b0ec51d3b3057a76dfd39da2a4ceadf90c38d123
Sinval Borges <sinborneto@yahoo.com.br>
2025-01-16 14:41:38 -0300

    separando componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: AppComponent,
         pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
       }
   ];
